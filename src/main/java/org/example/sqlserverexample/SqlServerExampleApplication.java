3ef55c243591a5166548231ee0e5f448077ee573
Orges Balla <orges.balla@lhind.dlh.de>
2024-08-14 08:35:21 +0200

    first commit

---
+++ src/main/java/org/example/sqlserverexample/SqlServerExampleApplication.java
@@ -0,0 +1,13 @@
+package org.example.sqlserverexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlServerExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlServerExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sqlserverexample/SqlServerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sqlserverexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlServerExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
